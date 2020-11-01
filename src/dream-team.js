c67624c426b299646a1129aff2f4f517792e592f
asus <aaa@aaa.aaa>
2020-11-01 14:03:08 +0500

    feat: dream-team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(members == null) return false;
+  let a = [];
+
+  for(let i = 0; i<members.length; i++){
+    if(typeof members[i] != 'string'){}
+    else{
+      let word = members[i].toUpperCase().replace(/\s+/g, '');
+      a.push(word[0]);
+    }
+  } 
+  a.sort();
+  return a.join('');
+
 };
